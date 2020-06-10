6b8a33bd6f8b7008c415b5be44bf0bc290c4c9a6
jiumengchongshi <jiuguihuasheng@163.com>
2020-06-10 21:08:33 +0800

    Python 网站开发(5) -- 搭建Web框架

--- www/apis.py
+++ www/apis.py
@@ -23,4 +23,25 @@ class Page(object):
     __repr__ = __str__
 
 ## 以下为API的几类错误代码
+class APIError(Exception):
+    def __init__(self, error, data='', message=''):
+        super(APIError, self).__init__(message)
+        self.error = error
+        self.data = data
+        self.message = message
 
+class APIValueError(APIError):
+    def __init__(self, field, message=''):
+        super(APIValueError, self).__init__('value:invalid', field, message)
+
+class APIResourceNotFoundError(APIError):
+    def __init__(self, field, message=''):
+        super(APIResourceNotFoundError, self).__init__('value:notfound', field, message)
+
+class APIPermissionError(APIError):
+    def __init__(self, message=''):
+        super(APIPermissionError, self).__init__('permission:forbidden', 'permission', message)
+
+if __name__ == '__main__':
+    import doctest
+    doctest.testmod()
--- www/coroweb.py
+++ www/coroweb.py
@@ -131,7 +131,7 @@ class RequestHandler(object):
             for name in self._required_kw_args:
                 if not name in kw:
                     return web.HTTPBadRequest(text='Missing argument: %s' % name)
-        logging.info('call with args: %s' str(kw))
+        logging.info('call with args: %s' % str(kw))
         try:
             r = await self._func(**kw)
             return r
